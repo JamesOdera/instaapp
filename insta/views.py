3e5b1c5201f7e2692a9a191625cf639bd18f4d22
JamesOdera <nyangoma.odera@gmail.com>
2020-03-06 20:49:53 +0300

    start a new app to handle our regitration for users

--- insta/views.py
+++ insta/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'insta/index.html')
+
+def about(request):
+    return render(request, 'insta/about.html')
