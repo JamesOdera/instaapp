5da3b93be67c8b64ec668102290ebf7796be4dd7
JamesOdera <nyangoma.odera@gmail.com>
2020-03-07 22:48:46 +0300

    make migrations for our images to display

--- insta/models.py
+++ insta/models.py
@@ -1,14 +1,28 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
+
+
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'image/')
     name = models.CharField(max_length =60)
     caption = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    
 
     def save_image(self):
         self.save()
 
     def delete_image(self):
         self.delete()
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User,  on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- insta/views.py
+++ insta/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as dt
-from .models import Image
+from .models import Image, Post
 
 
 def index(request):
     date = dt.date.today()
     images= Image.objects.all()
-    return render(request, 'insta/index.html', {"date": date,"images": images})
+    posts= Post.objects.all()
+    return render(request, 'insta/index.html', {"date": date,"images": images, "posts":posts})
 
 def about(request):
     return render(request, 'insta/about.html')
+    
