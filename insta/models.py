5d615b36010023669711b63011af4dd31dd98d43
JamesOdera <nyangoma.odera@gmail.com>
2020-03-07 19:04:23 +0300

    connect to database and update the database in settings pyfile

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'image/')
+    name = models.CharField(max_length =60)
+    caption = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
--- insta/views.py
+++ insta/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
+from .models import Image
+
 
 def index(request):
-    return render(request, 'insta/index.html')
+    date = dt.date.today()
+    images= Image.objects.all()
+    return render(request, 'insta/index.html', {"date": date,"images": images})
 
 def about(request):
     return render(request, 'insta/about.html')
