1ddc55f823f7cb522f719ac2652941f4ecd05ff8
JamesOdera <nyangoma.odera@gmail.com>
2020-03-08 19:25:04 +0300

    add the ability to add images on a post

--- insta/models.py
+++ insta/models.py
@@ -23,6 +23,7 @@ class Post(models.Model):
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User,  on_delete=models.CASCADE)
+    cover = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.title
